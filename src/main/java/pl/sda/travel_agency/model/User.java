9258a9e192e9ee3b57f11357e8373446c2a0200e
Tomasz Politowski <tomasz.politowski.tp@gmail.com>
2019-06-23 11:21:52 +0200

    added User and Role

--- src/main/java/pl/sda/travel_agency/model/User.java
+++ src/main/java/pl/sda/travel_agency/model/User.java
@@ -1,4 +1,51 @@
 package pl.sda.travel_agency.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String lastName;
+    private String email;
+    private String password;
+
+
+    private boolean active = true;
+
+
+    @ManyToMany(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    public User(String name, String lastName, String email, String password) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void addRole(Role role){ this.roles.add(role);    }
 }
