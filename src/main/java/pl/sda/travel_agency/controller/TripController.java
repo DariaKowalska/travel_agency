e249ba30800ec6d6658c26f746e923ea38824540
Dell <asierzan@gmail.com>
2019-07-06 09:36:40 +0200

    add form buy trip

--- src/main/java/pl/sda/travel_agency/controller/TripController.java
+++ src/main/java/pl/sda/travel_agency/controller/TripController.java
@@ -1,6 +1,7 @@
 package pl.sda.travel_agency.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
